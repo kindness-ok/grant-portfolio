29d7087637513cc1f2ab58644cba368f2f875d35
Owieoghene16 <kindnessokpugie1@gmail.com>
2024-03-26 14:56:57 +0100

    adding functionalities to the application: part three

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,22 +1,30 @@
+import { useState } from 'react';
+
 const useUtilities = () => {
   const [emailText, setEmailText] = useState('');
   const [nameText, setNameText] = useState('');
   const [messageText, setMessageText] = useState('');
 
-  const handleNameChange = (e) => {
+  const handleNameChange = (e: Event) => {
     setNameText(e.target.value);
   };
-  const handleEmailChange = (e) => {
+  const handleEmailChange = (e: Event) => {
     setEmailText(e.target.value);
   };
-  const handleMessageChange = (e) => {
+  const handleMessageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setMessageText(e.target.value)
   };
   const openInNewTab = (url: string) => {
     window.open(url, '_blank', 'noopener,noreferrer');
   };
   return {
-    openInNewTab
+    nameText,
+    emailText,
+    messageText,
+    handleNameChange,
+    handleEmailChange,
+    handleMessageChange,
+    openInNewTab,
   }
 };
 
