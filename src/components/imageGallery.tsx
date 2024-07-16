e5e29973450898e8760a56393b4b02c322479eed
Owieoghene16 <kindnessokpugie1@gmail.com>
2024-07-16 13:14:14 +0100

    adding finishing touches: part eleven

--- src/components/imageGallery.tsx
+++ src/components/imageGallery.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Image from "next/image";
 import { Fade } from "react-awesome-reveal";
+import DesignOne from "../../public/gservice1.jpg";
+import DesignTwo from "../../public/gservice2.jpg";
 
 export default function ImageGallery () {
   return (
@@ -9,12 +11,12 @@ export default function ImageGallery () {
         <div className="grid gap-2 md:gap-4">
           <div>
             <Fade direction="up" duration={1000} delay={200} triggerOnce>
-              <Image sizes="100vw" width={0} height={0} style={{ width: '100%', height: 'auto' }} className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt="imagegrid"/>
+              <Image sizes="100vw" width={0} height={0} style={{ width: '100%', height: 'auto' }} className="h-auto max-w-full rounded-lg" src={DesignOne} alt="imagegrid"/>
             </Fade>
           </div>
           <div>
             <Fade direction="up" duration={1000} delay={200} triggerOnce>
-            <Image sizes="100vw" width={0} height={0} style={{ width: '100%', height: 'auto' }} className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt="imagegrid"/>
+            <Image sizes="100vw" width={0} height={0} style={{ width: '100%', height: 'auto' }} className="h-auto max-w-full rounded-lg" src={DesignTwo} alt="imagegrid"/>
             </Fade>
           </div>
           <div>
